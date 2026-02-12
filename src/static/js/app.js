dcb19b89d1fd281b4efed253e5a1431787f497fe
Minol Prageesa Fernando <63942152+Minolprageesha@users.noreply.github.com>
2026-02-12 16:53:03 +0000

    change the test

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">hi this is minol for test application!</p>
+                <p className="text-center">this is minol from dbs</p>
             )}
             {items.map(item => (
                 <ItemDisplay
